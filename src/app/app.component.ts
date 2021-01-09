d392bbe9e7d34193a3c926c73b616474492ff1e6
Chris Mason <m1achris43@gmail.com>
2021-01-09 15:41:11 -0500

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouteConfigLoadEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'plus-minus-ng';
+  display = 0;
+
+  minus() {
+    this.display--;
+  }
+
+  plus() {
+    this.display++;
+  }
 }
